7882b915a9098789e220e88cf83c43a2238a75ae
woshigerunze <grzaiyxr@icloud.com>
2020-07-14 15:13:22 +0800

    完全背包

---
+++ ACoj/03.cpp
@@ -0,0 +1,21 @@
+//
+// Created by 葛润泽 on 2020/7/14.
+//
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> weight{1,2,3,4};
+    vector<int> value{2,4,4,5};
+    int volume=6;
+    vector<int> dp(volume+1,0);
+    for(int i=0;i<weight.size();i++)
+    {
+        for(int j=weight[i];j<=volume;j++) {
+            dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
+        }
+    }
+    for(auto& elem : dp)cout<<elem<<" ";
+}
+
